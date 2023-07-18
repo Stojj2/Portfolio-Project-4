a7e43dc5298e79503603e2f5a6aa5826c21cc815
Stojj2 <stojj2@hotmail.com>
2023-07-18 09:31:20 +0000

    Initial commit, installing Django

---
+++ HAIR_SALON/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for HAIR_SALON project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HAIR_SALON.settings')
+
+application = get_asgi_application()
