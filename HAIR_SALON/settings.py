a704f067d2c5ae9f928f44c81c650c344e2ac21d
Stojj2 <stojj2@hotmail.com>
2023-07-18 14:30:04 +0000

    Adding static files to root with collectstatic command

--- HAIR_SALON/settings.py
+++ HAIR_SALON/settings.py
@@ -127,7 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 
 
