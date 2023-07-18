f9b3bf1c9e3b141f1ab8faff62ad3f19ff2c3346
Stojj2 <stojj2@hotmail.com>
2023-07-18 14:22:54 +0000

    Change DEBUG False in settings.py

--- HAIR_SALON/settings.py
+++ HAIR_SALON/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-stojj2-portfolioproject-22g6lac0ymf.ws-eu101.gitpod.io','hairsalonpp4-84f80a2eef1c.herokuapp.com']
 
