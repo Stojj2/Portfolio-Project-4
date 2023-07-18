bffe82575b0e875e33b5c41e3e330320172b989d
Stojj2 <stojj2@hotmail.com>
2023-07-18 10:35:15 +0000

    Add Postgres database and installed dj-database-url

--- HAIR_SALON/settings.py
+++ HAIR_SALON/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
 if os.path.isfile("env.py"):
     import env
 
@@ -77,13 +78,16 @@ WSGI_APPLICATION = 'HAIR_SALON.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
